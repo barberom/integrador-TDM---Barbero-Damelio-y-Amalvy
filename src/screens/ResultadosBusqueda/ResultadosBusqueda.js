f1e6de8cd5b4130937d0e31aa9353e939c4a8eef
Ramiamalvy1 <ramalvydegreef@udesa.edu.ar>
2026-04-13 16:46:33 -0300

    screen resultados

--- src/screens/ResultadosBusqueda/ResultadosBusqueda.js
+++ src/screens/ResultadosBusqueda/ResultadosBusqueda.js
@@ -1,4 +1,25 @@
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
 
+class Buscador extends Component{
+    constructor(props){
+    super(props);
+
+}
+ejecutarBusqueda(e){
+    e.preventdefault();
+    this.props.history.push("/Resultados/" + Buscador)
+}
+componentDidMount(){
+    
+}
+render(){
+    return (
+
+    )
+}
+
+}
 
 
 
