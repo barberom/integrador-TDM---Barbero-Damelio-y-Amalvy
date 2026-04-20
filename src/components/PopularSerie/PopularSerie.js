569e8d269a596bb31856ff500e8ff77b4201202b
ndamelio-sketch <ndamelio@udesa.edu.ar>
2026-04-20 19:31:13 -0300

    Favoritos para el componente PopularSerie

--- src/components/PopularSerie/PopularSerie.js
+++ src/components/PopularSerie/PopularSerie.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import { Link } from "react-router-dom";
-import "../Popular/Popular.css";
 import "./PopularSerie.css";
 
 class PopularSerie extends Component{
@@ -10,6 +9,26 @@ class PopularSerie extends Component{
             descripcion: "mostrar",
             clase: "hide",
             detalle: "Ver más",
+            estadoinv: 's',
+            estado2: 'h',
+            esFavorito: false,
+        }
+    }
+
+    componentDidMount(){
+        let storage = localStorage.getItem('favoritos');
+        let storageParse = JSON.parse(storage);
+        let filtro = [];
+        if ( storageParse && storageParse.length > 0){
+            filtro = storageParse.filter((e) => e.id === this.props.serie.id);
+        }
+
+        if (filtro.length > 0) {
+            this.setState({
+                estado2: 's',
+                estadoinv: 'h',
+                esFavorito: true,
+            })
         }
     }
 
@@ -30,6 +49,40 @@ class PopularSerie extends Component{
         }
     }
 
+    agregarFav(){
+        let storage = localStorage.getItem('favoritos');
+        let storageParse = JSON.parse(storage);
+        if (storageParse && this.state.esFavorito === false) {
+            storageParse.push({"id": this.props.serie.id, "tipo": "tv"});
+            localStorage.setItem('favoritos', JSON.stringify(storageParse));
+        }
+        else {
+            localStorage.setItem('favoritos', JSON.stringify([{"id": this.props.serie.id, "tipo": "tv"}]));
+        }
+        this.setState({
+            estado2: 's',
+            estadoinv: 'h',
+            esFavorito: true,
+        })
+        {console.log(localStorage)}
+    }
+
+    sacarFav(){
+        this.setState({
+            estado2: 'h',
+            estadoinv: 's',
+            esFavorito: false,
+        })
+        let storage = localStorage.getItem('favoritos');
+        let storageParse = JSON.parse(storage);
+        if (storageParse.length != 0) {
+            let filtro = storageParse.filter((e) => e.id !== this.props.serie.id);
+            localStorage.setItem('favoritos', JSON.stringify(filtro));
+            {console.log(localStorage)}
+        }
+
+    }
+
     render(){
         return(
             <article className='popular'>
@@ -39,7 +92,8 @@ class PopularSerie extends Component{
                 <div className="botones-home">
                     <button onClick={()=>this.verDescripcion()}>{this.state.detalle}</button>
                     <Link to={`/detalleS/${this.props.serie.id}`}>Ir a detalle</Link>
-                    <button>🩶</button>
+                    <button className={this.state.estadoinv} onClick={() => this.agregarFav()}>🩶</button>
+                    <button className={this.state.estado2} onClick={() => this.sacarFav()}>❤️</button>
                 </div>
             </article>
         )
